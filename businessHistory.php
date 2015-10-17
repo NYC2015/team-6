f9e7da585afeb0ea5dce92136c186653b90aaa42
swathiv17 <swathiv17@yahoo.com>
2015-10-17 05:36:29 -0400

    Create businessHistory.php

---
+++ businessHistory.php
@@ -0,0 +1,19 @@
+<php>
+	<head>
+	<h1> Transaction History </h1>
+	<body>
+	   <list> Grain Left (lbs.): <div> </div><!--empty container here-->
+           <br>
+	        Fruits/Vegetable (lbs.): <div></div> 
+            <br>
+               Dairy (lbs.): <div></div>
+            <br>
+               Meats (lbs.): <div></div>
+            <br>
+               Fish (lbs.): <div></div>
+            <br>
+            
+        </list>
+	</body>
+	</head>
+</php>	
